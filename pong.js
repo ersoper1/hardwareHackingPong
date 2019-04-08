f090af85a502ca50226002f23d6c6de56e870032
ersoper1 <48351311+ersoper1@users.noreply.github.com>
2019-04-08 11:47:08 +0100

    Update pong.js
    edited paddle speed

--- pong.js
+++ pong.js
@@ -25,16 +25,16 @@ function startBall() {
 };
 document.addEventListener('keydown', function (e) {
      if (e.keyCode == 87 || e.which == 87) { // W key
-      speedOfPaddle1 = -10;
+      speedOfPaddle1 = -20;
      }
      if (e.keyCode == 83 || e.which == 83) { // S Key
-      speedOfPaddle1 = 10;
+      speedOfPaddle1 = 20;
      }
      if (e.keyCode == 73 || e.which == 73) { // I arrow
-      speedOfPaddle2 = -10;
+      speedOfPaddle2 = -20;
      }
      if (e.keyCode == 75 || e.which == 75) { // K arrow
-      speedOfPaddle2 = 10;
+      speedOfPaddle2 = 20;
      }
 }, false);
 document.addEventListener('keyup', function (e) {
